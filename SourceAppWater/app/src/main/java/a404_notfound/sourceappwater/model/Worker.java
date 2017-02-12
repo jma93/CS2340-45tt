5bb0b638ac434758919d15ab23c4f2783661893d
Lase <lbayudan@gatech.edu>
2017-02-12 11:24:45 -0500

    Make class Worker

--- SourceAppWater/app/src/main/java/a404_notfound/sourceappwater/model/Worker.java
+++ SourceAppWater/app/src/main/java/a404_notfound/sourceappwater/model/Worker.java
@@ -3,8 +3,8 @@ package a404_notfound.sourceappwater.model;
 /**
  * Created by Lase on 2/12/2017.
  *
- * A registered user in the model.
+ * A worker in the model.
  */
 
-public class RegisteredUser {
+public class Worker {
 }
