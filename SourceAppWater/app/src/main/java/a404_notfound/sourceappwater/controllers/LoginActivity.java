8af49df0b639ebf84bb9c80deca11fc5b31c680b
ZTabassum <zohratab@gmail.com>
2017-02-21 16:23:57 -0500

    Added the cancel button

--- SourceAppWater/app/src/main/java/a404_notfound/sourceappwater/controllers/LoginActivity.java
+++ SourceAppWater/app/src/main/java/a404_notfound/sourceappwater/controllers/LoginActivity.java
@@ -99,7 +99,25 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+
+        //Sets event for when Cancel button is clicked
+        Button cancel_button = (Button) findViewById(R.id.cancel_bt);
+
+      cancel_button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Intent switchScreen = new Intent(getApplicationContext(), WelcomeActivity.class);
+                    startActivity(switchScreen);
+
+            }
+        });
+
+
     }
+
+
+
 
     private void populateAutoComplete() {
         if (!mayRequestContacts()) {
