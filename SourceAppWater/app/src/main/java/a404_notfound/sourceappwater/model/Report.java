70f1e1a850866a7d065603a5ca79effb03935e39
Michelle Ni <michelle_ni@yahoo.com>
2017-02-27 16:53:56 -0500

    Made Abstract class Report

---
+++ SourceAppWater/app/src/main/java/a404_notfound/sourceappwater/model/Report.java
@@ -0,0 +1,14 @@
+package a404_notfound.sourceappwater.model;
+
+/**
+ * Created by Michelle on 2/27/2017.
+ */
+
+public abstract class Report {
+    //someone please double check these types
+    private int[] date;
+    private int[] time;
+    private int reportNumber;
+    private String reporter;
+    private int[] coordinates;
+}
