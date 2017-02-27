120262658c9c20d8e861da9a10c7035b57b0c234
vylos <jma93@gatech.edu>
2017-02-27 17:10:48 -0500

    Added WaterTreatble enum, created waterreport class

--- SourceAppWater/app/src/main/java/a404_notfound/sourceappwater/model/Report.java
+++ SourceAppWater/app/src/main/java/a404_notfound/sourceappwater/model/Report.java
@@ -8,7 +8,13 @@ public abstract class Report {
     //someone please double check these types
     private int[] date;
     private int[] time;
-    private int reportNumber;
+    private static int reportNumber;
     private String reporter;
     private int[] coordinates;
+    public Report (String reporter, int[] coordinates) {
+        this.reporter = reporter;
+        this.coordinates = coordinates;
+        //add date and time from fire base as default.
+        reportNumber++;
+    }
 }
